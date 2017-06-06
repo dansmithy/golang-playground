c4bd980e3075175a1ffc330e12330d73d4bf1102
Danny Smith <danny.j.smith@oracle.com>
2017-06-06 14:41:17 +0100

    Getting clearer now

--- anonymous.go
+++ anonymous.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 )
 
-type IBase interface {
+// see https://stackoverflow.com/questions/24537443/meaning-of-a-struct-with-embedded-anonymous-interface for interesting discussion around aspects of this
+
+type IGreet interface {
 	hello() string
 }
 
-type IMoreGreet interface {
-	IBase
+type ISalutation interface {
+	IGreet
 	goodbye() string
 }
 
@@ -27,8 +29,12 @@ type BigPerson struct {
 	Age int
 }
 
-type LazyPerson struct {
-	IMoreGreet
+type CoughingPerson struct {
+	ISalutation
+}
+
+func (coughingPerson CoughingPerson) hello() string {
+	return "*cough* " + coughingPerson.ISalutation.hello() + " *cough*"
 }
 
 func (p Person) hello() string {
@@ -47,27 +53,43 @@ func (a Animal) goodbye() string {
 	return "Get lost you cheeky " + a.Species
 }
 
-func sayStuff(base IMoreGreet) {
+func giveCough(salutationer ISalutation) ISalutation {
+	return &CoughingPerson{salutationer}
+}
+
+func sayStuff(base ISalutation) {
 	fmt.Println(base.hello())
 	fmt.Println(base.goodbye())
 }
 
 func main() {
+
 	person := Person{"derek", "spink"}
-	sayStuff(person)
+	sayStuff(person) // runs the Person methods for hello/goodbye
+
+	fmt.Println("---------------------------")
 
 	animal := Animal{"cat"}
-	sayStuff(animal)
+	sayStuff(animal) // runs the Animal methods for hello/goodbye
+
+	fmt.Println("---------------------------")
 
 	bigPerson := BigPerson{Person{"clive", "tweebly"}, 50}
 	//   bigPerson := BigPerson{FirstName: "reginald", LastName: "boo", Age: 60} // can't create a BigPerson like this
-
-	fmt.Printf("We can also get the person out! %+v\n", bigPerson.Person)
-	sayStuff(bigPerson)
-
+	sayStuff(bigPerson)                          // since BigPerson is also a Person, will just run the Person hello/goodbye methods
 	fmt.Println("oh no! " + bigPerson.FirstName) // but can refer to field of a bigPerson like this
+	fmt.Printf("We can also get the person out! %+v\n", bigPerson.Person)
 
-	//lazyPerson := LazyPerson{}
-	// sayStuff(lazyPerson) // compiles (as LazyPerson has IMoreGreet as anonymous interface) but panics at runtime as there is no implementation
+	fmt.Println("---------------------------")
+
+	bigCoughingPerson := giveCough(bigPerson)
+	sayStuff(bigCoughingPerson) // now uses the CoughingPerson method for hello(), which adds some coughs
+
+	fmt.Println("---------------------------")
+
+	coughingSomething := CoughingPerson{}
+	// sayStuff(coughingSomething) // compiles (as CoughingPerson has ISalutation as anonymous interface) but panics at runtime as there is no implementation for ISalutation cos coughingSomething was not instantiated with an ISalutation instance
+	coughingSomething.ISalutation = animal
+	sayStuff(coughingSomething) // now is fine as we've given it an ISalutation instance.
 
 }
